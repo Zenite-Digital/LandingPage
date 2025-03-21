10522306e88dd242cbf57bcd85f910c8abb67efd
Vitor Garcia <vitorpso270@gmail.com>
2025-03-21 19:41:56 -0300

    [ FEAT ] Estilização dos botões do Header e do botão de temas

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,3 @@
-const svgs = {
-	sun: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="var(--theme-icon-color)" d="M12 5q-.425 0-.712-.288T11 4V2q0-.425.288-.712T12 1t.713.288T13 2v2q0 .425-.288.713T12 5m4.95 2.05q-.275-.275-.275-.687t.275-.713l1.4-1.425q.3-.3.712-.3t.713.3q.275.275.275.7t-.275.7L18.35 7.05q-.275.275-.7.275t-.7-.275M20 13q-.425 0-.713-.288T19 12t.288-.712T20 11h2q.425 0 .713.288T23 12t-.288.713T22 13zm-8 10q-.425 0-.712-.288T11 22v-2q0-.425.288-.712T12 19t.713.288T13 20v2q0 .425-.288.713T12 23M5.65 7.05l-1.425-1.4q-.3-.3-.3-.725t.3-.7q.275-.275.7-.275t.7.275L7.05 5.65q.275.275.275.7t-.275.7q-.3.275-.7.275t-.7-.275m12.7 12.725l-1.4-1.425q-.275-.3-.275-.712t.275-.688t.688-.275t.712.275l1.425 1.4q.3.275.288.7t-.288.725q-.3.3-.725.3t-.7-.3M2 13q-.425 0-.712-.288T1 12t.288-.712T2 11h2q.425 0 .713.288T5 12t-.288.713T4 13zm2.225 6.775q-.275-.275-.275-.7t.275-.7L5.65 16.95q.275-.275.687-.275t.713.275q.3.3.3.713t-.3.712l-1.4 1.4q-.3.3-.725.3t-.7-.3M12 18q-2.5 0-4.25-1.75T6 12t1.75-4.25T12 6t4.25 1.75T18 12t-1.75 4.25T12 18"/></svg>`,
-	moon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="var(--theme-icon-color)" d="m15 8l-3-3l3-3l3 3zm5 3l-2-2l2-2l2 2zm-7.925 11q-2.1 0-3.937-.8t-3.2-2.162t-2.163-3.2t-.8-3.938q0-3.65 2.325-6.437T10.225 2q-.45 2.475.275 4.838t2.5 4.137t4.138 2.5t4.837.275q-.65 3.6-3.45 5.925T12.075 22"/></svg>`,
-	lightZenite: ``,
-};
-
-let lightTheme;
-
-function init() {
-	const themeTogglerBtn = document.getElementById("theme-toggler-btn");
-
-	function handleChangeTheme() {
-		lightTheme = !lightTheme;
-		document.body.classList.add(lightTheme ? "light-theme" : "dark-theme");
-		document.body.classList.remove(!lightTheme ? "light-theme" : "dark-theme");
-
-		themeTogglerBtn.innerHTML = lightTheme ? svgs.sun : svgs.moon;
-	}
-
-	themeTogglerBtn.onclick = handleChangeTheme;
-
-	handleChangeTheme();
-}
+function init() {}
 
 window.addEventListener("load", init);
