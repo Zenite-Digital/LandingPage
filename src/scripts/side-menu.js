d9c7cddaefa14f5db9dc3579a66b0c32dcf7f8c0
Vitor Garcia <vitorpso270@gmail.com>
2025-04-28 18:21:10 -0300

    [ FEAT ] Adição de backdrop ao Side menu

--- src/scripts/side-menu.js
+++ src/scripts/side-menu.js
@@ -1,22 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() { 
+document.addEventListener("DOMContentLoaded", function () {
+    const sideMenuContainer = document.getElementById(
+        "side-menu-nav-container"
+    );
     const toggleSideMenuBtn = document.getElementById("toggle-side-menu-btn");
     const sideMenu = document.getElementById("side-menu");
-    
-    toggleSideMenuBtn.addEventListener("click", function() {
+
+    toggleSideMenuBtn.addEventListener("click", function () {
         if (sideMenu.style.width === "0px" || sideMenu.style.width === "") {
+            sideMenuContainer.style.display = "block";
             openNav();
         } else {
             closeNav();
         }
     });
-})
+});
 
-function openNav(){
+function openNav() {
+    document.getElementById("side-menu-nav-container").style.display = "block";
     document.getElementById("side-menu").style.width = "280px";
     document.getElementById("side-menu").style.transition = "width 0.5s";
 }
 
-function closeNav(){
+function closeNav() {
+    document.getElementById("side-menu-nav-container").style.display = "none";
     document.getElementById("side-menu").style.width = "0";
     document.getElementById("side-menu").style.transition = "width 0.5s";
 }
+
+window.SideMenu = {
+    closeNav,
+};
